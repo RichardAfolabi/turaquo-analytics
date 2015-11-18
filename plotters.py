846d0610a7f4c29f62be9d25799a0f67295a5cde
Afolabi, O. Richard <mailme@richardafolabi.com>
2015-11-18 01:02:15 -0800

    updated

--- plotters.py
+++ plotters.py
@@ -9,5 +9,5 @@ def plot_stacked_bar_chart(df, label_x, label_y, my_colors, is_stacked=False, lg
     :return:Returns the bar chart object for the input dataframe.
     """
     bar = Bar(df, df.index.tolist(), stacked=is_stacked, responsive=True, legend=lg_pos,
-              height=550, width=950, tools=TOOLS, xlabel=label_x, ylabel=label_y, palette=my_colors)
+              height=400, width=950, tools=TOOLS, xlabel=label_x, ylabel=label_y, palette=my_colors)
     return bar
